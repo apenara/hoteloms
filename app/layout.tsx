0012ea7223ba317c1bd5b6396a52331f1ebb2a84
Alfonso Penaranda <apenara.van@gmail.com>
2025-02-07 20:03:33 -0500

    se crea el manifest para el PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-import './ui/globals.css'
-import type { Metadata } from 'next'
+import "./ui/globals.css";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import { AuthProvider } from '@/lib/auth'; 
+import { AuthProvider } from "@/lib/auth";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HotelOms',
-  description: 'Sistema de Gestión Hotelera',
+  title: "HotelOms",
+  description: "Sistema de Gestión Hotelera",
+  manifest: "/manifest.json",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/favicon.ico',
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
   },
-}
-
+};
+export const viewport: Viewport = {
+  maximumScale: 1,
+  userScalable: false,
+};
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
-
       <body className={`${inter.className} antialiased`}>
-        <AuthProvider>
-      {children}
-      </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
-  )
+  );
 }
