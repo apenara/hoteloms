809da929b29e7943c24255d79d66f1c45f0d93f8
alfonso Penaranda <apenara.van@gmail.com>
2025-02-08 14:03:39 -0500

    elimina los logos antiguos y agrega nuevos íconos para mejorar la apariencia de la aplicación

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,6 @@ export default {
   },
   plugins: [
     require("tailwindcss-animate"),
-    require("tailwindcss-displaymodes"),
+    // require("tailwindcss-displaymodes"),
   ],
 } satisfies Config;
